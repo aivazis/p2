db783cda64719afb26d357595c6300471fa7c93f
Michael Aïvázis <michael.aivazis@para-sim.com>
2019-12-04 08:41:36 -0800

    p2: repaired the attributions

--- .mm/p2.mm
+++ .mm/p2.mm
@@ -1,9 +1,7 @@
 # -*- Makefile -*-
 #
-# michael a.g. aïvázis
-# orthologue
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
 # (c) 1998-2019 all rights reserved
-#
 
 # project meta-data
 p2.major := $(repo.major)
--- pkg/p2/__init__.py
+++ pkg/p2/__init__.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 #
-# michael a.g. aïvázis
-# parasim
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
 # (c) 1998-2019 all rights reserved
-#
 
 
 # publish
