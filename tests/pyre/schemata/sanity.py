de28d79bc507edbd5106f89158b42a7c9fadb40d
Michael Aïvázis <michael.aivazis@para-sim.com>
2019-12-07 11:32:22 -0800

    pyre.schemata: primed the package and its testsuite

---
+++ tests/pyre/schemata/sanity.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2019 all rights reserved
+
+
+# make sure the package is accessible
+import p2.schemata
+
+
+# end of file
