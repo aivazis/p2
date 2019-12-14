d93341a1dc6ecc65a82e1080a308f7cfd34f215a
Michael Aïvázis <michael.aivazis@para-sim.com>
2019-12-14 14:23:24 -0800

    pyre.components: fleshed out a bit more of {Configurable}

--- pkg/p2/components/Configurable.py
+++ pkg/p2/components/Configurable.py
@@ -4,11 +4,232 @@
 # (c) 1998-2019 all rights reserved
 
 
+# externals
+import itertools
+
+
 # declaration
 class Configurable:
     """
     The base class for components and protocols
     """
+
+    # framework data;
+    # structural
+    pyre_pedigree = ()         # ancestors that are configurables, in mro
+    pyre_traits = ()           # all traits, both local and inherited
+    pyre_localTraits = ()      # traits from my declaration
+    pyre_inheritedTraits = ()  # inherited traits
+    # name maps
+    pyre_namemap = None        # map of trait aliases to their canonical name
+    pyre_traitmap = None       # map of canonical trait names to their descriptor
+    # convenience
+    pyre_locator = None        # the location of the configurable instantiation
+    # predicates that avoid runtime type identification
+    pyre_isProtocol = False    # am i a protocol?
+    pyre_isComponent = False   # am i a component?
+    # misc
+    pyre_isInternal = True     # am i visible to end-users
+
+
+    # interface
+    @classmethod
+    def pyre_trait(cls, alias):
+        """
+        Given the {alias}, locate and return the associated descriptor
+        """
+        # attempt to
+        try:
+            # use the {alias} to retrieve the canonical name
+            canonical = cls.pyre_namemap[alias]
+        # if this fails, {alias} is not a name for one of my traits
+        except KeyError:
+            # complain
+            raise cls.TraitNotFoundError(configurable=cls, name=alias)
+
+        # got it; attempt to
+        try:
+            # look through my trait map for the matching descriptor
+            trait = cls.pyre_traitmap[canonical]
+        # if it's not there
+        except KeyError:
+            # we have a bug; get the journal
+            import journal
+            # build a channel
+            channel = journal.firewall("pyre.components")
+            # and complain
+            raise channel.log(f"{cls}: missing trait for '{alias}' -> '{canonical}'")
+
+        # all done
+        return trait
+
+
+    # introspection
+    @classmethod
+    def pyre_traits(cls):
+        """
+        Generate a sequence of all my trait descriptors, both locally declared and inherited.
+
+        If you are looking only for traits declared in a particular class, use the attribute
+        {cls.pyre_localTraits} instead.
+        """
+        # the full set of my descriptors is prepared by {Requirement} and separated into two
+        # pile: my local traits that were explicitly declared in my class record, and traits i
+        # inherited from my ancestors
+        return itertools.chain(cls.pyre_localTraits, cls.pyre_inheritedTraits)
+
+
+    @classmethod
+    def pyre_behaviors(cls):
+        """
+        Generate a sequence of all my trait descriptors that are behaviors
+        """
+        return filter(lambda trait: trait.isBehavior, cls.pyre_traits())
+
+
+    @classmethod
+    def pyre_derivations(cls):
+        """
+        Generate a sequence of all my trait descriptors that are derivations
+        """
+        return filter(lambda trait: trait.isDerivation, cls.pyre_traits())
+
+
+    @classmethod
+    def pyre_measures(cls):
+        """
+        Generate a sequence of all my trait descriptors that are measures
+        """
+        return filter(lambda trait: trait.isMeasure, cls.pyre_traits())
+
+
+    @classmethod
+    def pyre_properties(cls):
+        """
+        Generate a sequence of all my trait descriptors that are properties
+        """
+        return filter(lambda trait: trait.isProperty, cls.pyre_traits())
+
+
+    @classmethod
+    def pyre_facilities(cls):
+        """
+        Generate a sequence of all my trait descriptors that are facilities
+        """
+        return filter(lambda trait: trait.isFacility, cls.pyre_traits())
+
+
+    @classmethod
+    def pyre_localBehaviors(cls):
+        """
+        Generate a sequence of my local trait descriptors that are behaviors
+        """
+        return filter(lambda trait: trait.isBehavior, cls.pyre_localTraits)
+
+
+    @classmethod
+    def pyre_localDerivations(cls):
+        """
+        Generate a sequence of my local trait descriptors that derivations
+        """
+        return filter(lambda trait: trait.isDerivation, cls.pyre_localTraits)
+
+
+    @classmethod
+    def pyre_localMeasures(cls):
+        """
+        Generate a sequence of my local trait descriptors that measures
+        """
+        return filter(lambda trait: trait.isMeasure, cls.pyre_localTraits)
+
+
+    @classmethod
+    def pyre_localProperties(cls):
+        """
+        Generate a sequence of my local trait descriptors that are properties
+        """
+        return filter(lambda trait: trait.isProperty, cls.pyre_localTraits)
+
+
+    @classmethod
+    def pyre_localFacilities(cls):
+        """
+        Generate a sequence of all my trait descriptors that are facilities
+        """
+        return filter(lambda trait: trait.isFacility, cls.pyre_localTraits)
+
+
+    @classmethod
+    def pyre_inheritedBehaviors(cls):
+        """
+        Generate a sequence of my inherited trait descriptors that are behaviors
+        """
+        return filter(lambda trait: trait.isBehavior, cls.pyre_inheritedTraits)
+
+
+    @classmethod
+    def pyre_inheritedDerivations(cls):
+        """
+        Generate a sequence of my inherited trait descriptors that derivations
+        """
+        return filter(lambda trait: trait.isDerivation, cls.pyre_inheritedTraits)
+
+
+    @classmethod
+    def pyre_inheritedMeasures(cls):
+        """
+        Generate a sequence of my inherited trait descriptors that measures
+        """
+        return filter(lambda trait: trait.isMeasure, cls.pyre_inheritedTraits)
+
+
+    @classmethod
+    def pyre_inheritedProperties(cls):
+        """
+        Generate a sequence of my inherited trait descriptors that are properties
+        """
+        return filter(lambda trait: trait.isProperty, cls.pyre_inheritedTraits)
+
+
+    @classmethod
+    def pyre_inheritedFacilities(cls):
+        """
+        Generate a sequence of all my trait descriptors that are facilities
+        """
+        return filter(lambda trait: trait.isFacility, cls.pyre_inheritedTraits)
+
+
+    # framework hooks
+    @classmethod
+    def pyre_classRegistered(cls):
+        """
+        Hook that gets invoked by the framework after the class record has been registered but
+        before any configuration events
+        """
+        # do nothing
+        return cls
+
+
+    @classmethod
+    def pyre_classConfigured(cls):
+        """
+        Hook that gets invoked by the framework after the class record has been configured,
+        before any instances have been created
+        """
+        # do nothing
+        return cls
+
+
+    @classmethod
+    def pyre_classInitialized(cls):
+        """
+        Hook that gets invoked by the framework after the class record has been initialized,
+        before any instances have been created
+        """
+        # do nothing
+        return cls
+
+
 
 
 # end of file
--- pkg/p2/components/Requirement.py
+++ pkg/p2/components/Requirement.py
@@ -16,4 +16,81 @@ class Requirement(AttributeFilter):
     """
 
 
+    # types
+    from ..traits.Trait import Trait
+    from .Configurable import Configurable
+
+
+    # metamethods
+    def __new__(cls, name, bases, attributes, *, internal=False, **kwds):
+        """
+        Build the class record for a new configurable
+        """
+        # build the record
+        configurable = super().__new__(cls, name, bases, attributes, **kwds)
+
+        # compute the pedigree
+        pedigree = tuple(base for base in configurable.mro()
+                         if issubclass(base, cls.Configurable) and base is not cls.Configurable)
+
+        # initialize storage for the name maps
+        namemap = {}  # aliases -> canonical name
+        traitmap = {} # canonical name -> descriptor
+        # initialize the trait piles
+        localTraits = []     # declared in the record we are building
+        inheritedTraits = [] # inherited from all ancestors
+
+        # scan the attributes for traits
+        for name, trait in cls.pyre_harvest(attributes=attributes, descriptor=cls.Trait):
+            # map the name to the trait
+            traitmap[name] = trait
+            # map all aliases to the canonical name; this includes the canonical name since
+            # trait binding happens during the creation of the class record
+            namemap.update((alias, name) for alias in trait.aliases)
+            # add the trait to the local pile
+            localTraits.append(trait)
+
+        # scan my ancestors for traits
+        #
+        # N.B.: this must be done carefully to account for trait shadowing where distant
+        # ancestor traits become inaccessible when closer ancestors declare a trait with the
+        # same name. resist the temptation to speed this up by traversing the pedigree in
+        # reverse order and throw everything in the map expecting that closer ancestors will
+        # update the maps correctly, because there is no easy way to undo the mapping of the
+        # aliases of a trait that get shadowed by a closer ancestor
+        for base in pedigree[1:]:
+            # and through each trait
+            for trait in base.pyre_localTraits:
+                # get the name of the trait
+                traitName = trait.name
+                # and its aliases
+                traitAliases = trait.aliases
+                # if the name is not shadowed
+                if traitName not in traitmap:
+                    # add it to the pile
+                    inheritedTraits.append(trait)
+                    # map its canonical name
+                    traitmap[traitName] = trait
+                    # go through its aliases
+                    for traitAlias in traitAliases:
+                        # if there is no existing entry
+                        if traitAlias not in namemap:
+                            # map the alias to the canonical name
+                            namemap[traitAlias] = traitName
+
+        # record the pedigree
+        configurable.pyre_pedigree = pedigree
+        # record the trait piles
+        configurable.pyre_localTraits = tuple(localTraits)
+        configurable.pyre_inheritedTraits = tuple(inheritedTraits)
+        # record the name maps
+        configurable.pyre_namemap = namemap
+        configurable.pyre_traitmap = traitmap
+        # mark the visibility of the record
+        configurable.pyre_isInternal = internal
+
+        # all done
+        return configurable
+
+
 # end of file
