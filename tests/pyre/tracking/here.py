a067a051b393308938102ffee658dbb87858d163
Michael Aïvázis <michael.aivazis@para-sim.com>
2019-12-21 09:17:23 -0800

    pyre.tracking: added a test for {here}

---
+++ tests/pyre/tracking/here.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2019 all rights reserved
+
+
+def test():
+    """
+    Verify that the location of the caller of a function is recorded correctly
+    """
+    # access the package
+    import p2.tracking
+
+    # make a locator for this statement
+    locator = p2.tracking.here()
+
+    # verify we got the correct type of locator
+    assert isinstance(locator, p2.tracking.script)
+    # and that everything was recorded correctly
+    assert locator.source == __file__
+    assert locator.line == 16
+    assert locator.function == 'test'
+
+    # now, look up my caller
+    locator = p2.tracking.here(level=1)
+    # verify we got the correct type of locator
+    assert isinstance(locator, p2.tracking.script)
+    # and that everything was recorded correctly
+    assert locator.source == __file__
+    assert locator.line == 43
+    assert locator.function == '<module>'
+
+    # all done
+    return locator
+
+
+# main
+if __name__ == "__main__":
+    # skip pyre initialization since we don't rely on the executive
+    pyre_noboot = True
+    # do...
+    test()
+
+
+# end of file
