c45fcb8cb7c244c28e331058ea52dfb1b2e31ba3
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-01-06 22:22:48 +0200

    pyre.framework: added a test case with an invalid custom {dashboard}

---
+++ tests/pyre/framework/dashboard_bad.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2020 all rights reserved
+
+
+def test():
+    """
+    Verify we can install a custom dashboard correctly
+    """
+    # attempt to
+    try:
+        # access the package
+        import p2
+    # if it fails
+    except:
+        # all good
+        pass
+
+    # all done
+    return
+
+
+# main
+if __name__ == "__main__":
+    # set the dashboard to an illegal value
+    pyre_dashboard = 0
+    # do...
+    test()
+
+
+# end of file
