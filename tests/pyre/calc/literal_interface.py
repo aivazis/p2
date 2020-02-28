a191b721fea66ec48d78c8f261fca62770235b1b
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-02-27 22:21:48 -0800

    pyre.calc: added test that exercises the {literal} interface

---
+++ tests/pyre/calc/literal_interface.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2020 all rights reserved
+
+
+def test():
+    """
+    Exercise the interface of literals
+    """
+    # get the base node form the {calc} package
+    from p2.calc.Node import Node as node
+
+    # make a literal
+    literal = node.literal(value=0)
+
+    # access the value
+    assert literal.getValue() == 0
+
+    # attempt to
+    try:
+        # set the value
+        literal.setValue(value=1)
+        # which should fail
+        assert False
+    # because {const} is protecting it
+    except NotImplementedError as error:
+        # all good
+        pass
+
+    # all done
+    return
+
+
+# main
+if __name__ == "__main__":
+    # run the test
+    test()
+
+
+# end of file
