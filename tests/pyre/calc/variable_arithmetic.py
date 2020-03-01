1052616054db715e56aa17fb68e1120a36dc9167
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-02-29 22:13:30 -0800

    pyre.calc: added arithmetic test for {variable}

---
+++ tests/pyre/calc/variable_arithmetic.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2020 all rights reserved
+
+
+def test():
+    """
+    Exercise basic arithmetic operations among variables
+    """
+    # get the base node form the {calc} package
+    from p2.calc.Node import Node as node
+
+    # make a couple of variables
+    v1 = node.variable(value=1)
+    v2 = node.variable(value=2)
+
+    # basic arithmetic among variables
+    assert (v1 + v2).getValue() == 3
+    assert (v1 - v2).getValue() == -1
+    assert (v1 * v2).getValue() == 2
+    assert (v1 / v2).getValue() == 0.5
+    assert (v1 ** v2).getValue() == 1
+    assert (v1 % v2).getValue() == 1
+
+    # variables and literals
+    assert (v1 + 2).getValue() == 3
+    assert (v1 - 2).getValue() == -1
+    assert (v1 * 2).getValue() == 2
+    assert (v1 / 2).getValue() == 0.5
+    assert (v1 ** 2).getValue() == 1
+    assert (v1 % 2).getValue() == 1
+
+    # all done
+    return
+
+
+# main
+if __name__ == "__main__":
+    # run the test
+    test()
+
+
+# end of file
