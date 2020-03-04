9d0c016ab04076645d903c0d1f383177663481ce
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-03-03 18:57:10 -0800

    pyre.calc: primed {probe}

---
+++ pkg/p2/calc/Probe.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2020 all rights reserved
+
+
+# probes watch nodes and participate in the event notification system
+class Probe:
+    """
+    The base class for non-node entities that observe {calc} nodes
+    """
+
+
+# end of file
