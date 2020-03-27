43d275dc189d5e696c12befe58c4e1f78fc34454
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-03-27 11:14:25 -0700

    journal: added missing {portinfo} include in {device}

--- lib/libj2/Device.cc
+++ lib/libj2/Device.cc
@@ -3,6 +3,9 @@
 // michael a.g. aïvázis <michael.aivazis@para-sim.com>
 // (c) 1998-2020 all rights reserved
 
+
+// for the build system
+#include <portinfo>
 // get the forward declarations
 #include "forward.h"
 // external support
