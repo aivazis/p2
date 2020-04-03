42d2b26817343b39ee67fb2913b5d1c83e45e6de
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-04-02 18:59:15 -0700

    journal: the {x11} color table is now a faithful repoduction of the official {rgb.txt}

--- lib/libj2/ANSI.cc
+++ lib/libj2/ANSI.cc
@@ -30,8 +30,8 @@ compatible { "ansi",
 // the helpers
 static bool emulates();
 
+// the static data
 static ansi_t::table_type make_ansi();
-static ansi_t::table_type make_x11();
 static ansi_t::table_type make_gray();
 static ansi_t::table_type make_misc();
 
@@ -49,7 +49,6 @@ bool ansi_t::compatible()
 // the static objects in {ansi_t}
 ansi_t::table_type ansi_t::null;
 ansi_t::table_type ansi_t::ansi = make_ansi();
-ansi_t::table_type ansi_t::x11 = make_x11();
 ansi_t::table_type ansi_t::gray = make_gray();
 ansi_t::table_type ansi_t::misc = make_misc();
 
@@ -103,39 +102,6 @@ ansi_t::table_type make_ansi()
     table["light-purple"] = csi_t::csi3(35, true);
     table["light-cyan"] = csi_t::csi3(36, true);
     table["white"] = csi_t::csi3(37, true);
-
-    // all done
-    return table;
-}
-
-
-ansi_t::table_type make_x11()
-{
-    // make a table
-    ansi_t::table_type table;
-
-    // the reset sequence
-    table["normal"] = csi_t::csi3(0);
-
-    // the X11 named colors
-    table["burlywood"] = csi_t::csi24(0xde, 0xb8, 0x87);
-    table["dark_goldenrod"] = csi_t::csi24(0xb8, 0x86, 0x0b);
-    table["dark_khaki"] = csi_t::csi24(0xbd, 0xb7, 0x6b);
-    table["dark_orange"] = csi_t::csi24(0xff, 0x8c, 0x00);
-    table["dark_sea_green"] = csi_t::csi24(0x8f, 0xbc, 0x8f);
-    table["firebrick"] = csi_t::csi24(0xb2, 0x22, 0x22);
-    table["hot_pink"] = csi_t::csi24(0xff, 0x69, 0xb4);
-    table["indian_red"] = csi_t::csi24(0xcd, 0x5c, 0x5c);
-    table["lavender"] = csi_t::csi24(0xc0, 0xb0, 0xe0);
-    table["light_green"] = csi_t::csi24(0x90, 0xee, 0x90);
-    table["light_steel_blue"] = csi_t::csi24(0xb0, 0xc4, 0xde);
-    table["light_slate_gray"] = csi_t::csi24(0x77, 0x88, 0x99);
-    table["lime_green"] = csi_t::csi24(0x32, 0xcd, 0x32);
-    table["navajo_white"] = csi_t::csi24(0xff, 0xde, 0xad);
-    table["olive_drab"] = csi_t::csi24(0x6b, 0x8e, 0x23);
-    table["peach_puff"] = csi_t::csi24(0xff, 0xda, 0xb9);
-    table["sage"] = csi_t::csi24(176, 208, 176);
-    table["steel_blue"] = csi_t::csi24(0x46, 0x82, 0xb4);
 
     // all done
     return table;
