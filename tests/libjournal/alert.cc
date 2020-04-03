0d970718b804d39152d0353baa4abce6324cc43f
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-04-03 16:01:44 -0700

    journal: added preliminary test cases for {memo} and {alert}; they are lit and noisy, for now

--- tests/libjournal/alert.cc
+++ tests/libjournal/alert.cc
@@ -11,7 +11,7 @@
 // type aliases
 using csi_t = pyre::journal::csi_t;
 using ansi_t = pyre::journal::ansi_t;
-using memo_t = pyre::journal::memo_t;
+using alert_t = pyre::journal::alert_t;
 using debug_t = pyre::journal::debug_t;
 using chronicler_t = pyre::journal::chronicler_t;
 
@@ -22,11 +22,11 @@ int main() {
     // grab the global metadata table from chronicler
     auto & globals = chronicler_t::globals();
     // set some metadata
-    globals["application"] = "memo";
+    globals["application"] = "alert";
     globals["author"] = "michael";
 
     // use a {debug_t} to build a document and its metdata
-    debug_t channel("memo");
+    debug_t channel("alert");
     // put some stuff in it; careful not to flush so we don't lose everything
     channel
         << pyre::journal::at(__HERE__)
@@ -34,11 +34,11 @@ int main() {
         << pyre::journal::set("device", "null")
         << "simon says:"
         << pyre::journal::newline
-        << "    hello world!"
+        << "hello world!"
         << pyre::journal::newline;
 
     // make a palette
-    memo_t::palette_type palette;
+    alert_t::palette_type palette;
     // add some decorations
     palette["reset"] = ansi_t::x11.at("normal");
     palette["channel"] = ansi_t::x11.at("light slate gray");
@@ -55,10 +55,10 @@ int main() {
     // and its metadata
     auto & meta = channel.metadata();
 
-    // make a memo
-    memo_t memo;
+    // make an alert
+    alert_t alert;
     // ask it to render what we have
-    auto msg = memo.render(palette, page, meta);
+    auto msg = alert.render(palette, page, meta);
 
     // show me
     std::cout << msg;
