c2b4976922780d017fb7f624461597da77caef88
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-04-05 00:07:10 -0700

    journal: added device testing logic to the {index} cascade test case

--- tests/libjournal/index_cascade.cc
+++ tests/libjournal/index_cascade.cc
@@ -11,8 +11,11 @@
 
 
 // type aliases
+// instantiate a functional index
 const bool defaultState = true;
 using index_t = pyre::journal::index_t<pyre::journal::inventory_t<defaultState>>;
+// the trash can
+using trash_t = pyre::journal::trash_t;
 
 
 // exercise the channel state index
@@ -24,19 +27,25 @@ int main() {
     auto & parent = index.lookup("test.index.parent");
     // make sure its default state is what we expect
     assert(parent.defaultState() == defaultState);
-    // and its actual state is what's expected
+    // its actual state is what's expected
     assert(parent.state() == defaultState);
+    // and the device is null
+    assert(parent.device() == nullptr);
     // turn it off
     parent.deactivate();
     // make sure it happened
     assert(parent.state() == false);
+    // set the device to a trash can
+    parent.device(std::make_shared<trash_t>());
 
     // lookup a name that's lower in the hierarchy
     auto & child = index.lookup("test.index.parent.blah.blah.child");
     // make sure its default state is what we expect
     assert(child.defaultState() == defaultState);
-    // and its actual state is what's expected
+    // its actual state is what's expected
     assert(child.state() == parent.state());
+    // and that it inherited the device
+    assert(child.device() == parent.device());
 
     // all done
     return 0;
