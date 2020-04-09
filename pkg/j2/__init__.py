ea996e5d3fdd485be9ec3e36c8964323d26eaab5
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-04-08 17:21:52 -0700

    journal: primed the python package

---
+++ pkg/j2/__init__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2020 all rights reserved
+
+
+# administrative
+def copyright():
+    """
+    Return the pyre copyright note
+    """
+    return print(meta.header)
+
+
+def license():
+    """
+    Print the pyre license
+    """
+    # print it
+    return print(meta.license)
+
+
+def version():
+    """
+    Return the pyre version
+    """
+    return meta.version
+
+
+def credits():
+    """
+    Print the acknowledgments
+    """
+    # print it
+    return print(meta.acknowledgments)
+
+
+# publish
+# the metadata
+from . import meta
+# the bindings
+from .ext import libjournal
+
+
+# end of file
