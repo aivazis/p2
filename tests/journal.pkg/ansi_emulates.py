203f138a164ac076b796b7c02c3f5dda0b3a5751
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-04-16 12:57:04 -0700

    journal: the {ansi} wrapper can now check whether the terminal understands its control sequences

---
+++ tests/journal.pkg/ansi_emulates.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2020 all rights reserved
+
+
+def test():
+    """
+    Check a few of the canonical X11 color names
+    """
+    # for the command line
+    import sys
+    # access the color map
+    from j2.ANSI import ANSI
+
+    # pull the expected value from the command line
+    expectation = bool(int(sys.argv[1])) if len(sys.argv) > 1 else True
+    # ask the color map
+    observation = ANSI.compatible()
+
+    # verify that the two match
+    assert expectation == observation
+
+    # all done
+    return
+
+
+# main
+if __name__ == "__main__":
+    # run the test
+    test()
+
+
+# end of file
