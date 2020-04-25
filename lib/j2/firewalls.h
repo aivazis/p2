f1b7c79d39b64d4e627ee1916ea9d3ba18f1f785
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-04-25 07:55:40 -0700

    journal: added C bindings for {firewall_t}

---
+++ lib/j2/firewalls.h
@@ -0,0 +1,28 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2020 all rights reserved
+
+#if !defined(pyre_journal_firewalls_h)
+#define pyre_journal_firewalls_h
+
+
+// the macros
+#include "macros.h"
+
+
+/* build the declarations of the bindings in a C-compatible way */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    void firewall_hit(const char * channel, __HERE_DECL__, const char * fmt, ...);
+    void firewall_check(const char * channel, int condition, __HERE_DECL__, const char * fmt, ...);
+
+#ifdef __cplusplus
+}
+#endif
+
+# endif // pyre_journal_firewalls_h
+
+/* end of file */
---
+++ tests/journal.lib/firewalls.c
@@ -0,0 +1,26 @@
+// -*- c -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2020 all rights reserved
+
+
+// support for {bool}
+#include <stdbool.h>
+// get the journal
+#include <p2/journal/firewalls.h>
+
+
+// exercise the C bindings
+int main() {
+    // name a channel
+    const char * channel = "tests.journal.debuginfo";
+
+    // check a true statement
+    firewall_check(channel, true, __HERE__, "something is %s", "wrong");
+
+    // all done
+    return 0;
+}
+
+
+// end of file
