3e9b1d26f0a00661cf3d9e1f39194e1762b6a601
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-05-14 15:29:11 -0700

    p2: activated the extension module

--- pkg/p2/ext/__init__.py
+++ pkg/p2/ext/__init__.py
@@ -4,4 +4,14 @@
 # (c) 1998-2020 all rights reserved
 
 
+# attempt to
+try:
+    # the journal bindings
+    from . import p2 as libpyre
+# if something goes wrong
+except ImportError:
+    # mark; the rest of the package will adjust
+    libpyre = None
+
+
 # end of file
