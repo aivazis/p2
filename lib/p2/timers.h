4ae88224b2a0abadb3d4bb29d7ac016c58df5048
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-05-14 15:30:32 -0700

    p2: added the main {timers} header file

---
+++ lib/p2/timers.h
@@ -0,0 +1,20 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2020 all rights reserved
+
+// code guard
+#if !defined(pyre_timers_h)
+#define pyre_timers_h
+
+
+//  the file you are looking for is "timers/api.h"; it contains the published typenames that
+//  constitute the {timers} public api
+
+// publish the interface
+#include "timers/public.h"
+
+
+#endif
+
+// end of file
