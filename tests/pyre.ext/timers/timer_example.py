cd01eed960956dc291e01acef0c54b01c6d38e26
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-05-16 09:09:25 -0700

    pyre::timers: added a couple of test cases to exercise the bindings

---
+++ tests/pyre.ext/timers/timer_example.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2020 all rights reserved
+
+
+def test():
+    """
+    Sanity test: make sure the timer bindings are accessible
+    """
+    # access the timer bindings
+    import p2.ext.p2 as libpyre
+
+    # make a timer
+    t = libpyre.Timer(name="tests.timer")
+    # start it
+    t.start()
+
+    # do something
+
+    # stop the timer
+    t.stop()
+
+    # get the journal
+    import journal
+    # make a channel
+    channel = journal.debug(name="pyre.timers")
+    # activate it
+    channel.activate()
+    # log the elapsed time
+    channel.log(f"elapsded: {t.ms()}")
+
+    # all done
+    return
+
+
+# main
+if __name__ == "__main__":
+    # run the test
+    test()
+
+
+# end of file
---
+++ tests/pyre.ext/timers/timer_instance.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2020 all rights reserved
+
+
+def test():
+    """
+    Sanity test: make sure the timer bindings are accessible
+    """
+    # access the timer bindings
+    import p2.ext.p2 as libpyre
+
+    # make a timer
+    t = libpyre.Timer(name="tests.timer")
+    # verify its name
+    assert t.name == "tests.timer"
+
+    # all done
+    return
+
+
+# main
+if __name__ == "__main__":
+    # run the test
+    test()
+
+
+# end of file
