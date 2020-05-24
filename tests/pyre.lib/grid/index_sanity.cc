ff0c9f22eff99ddc2c3acefb7846e96de0ffe00e
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-05-23 23:35:30 -0700

    pyre::grid: added sanity test for {index_t}

---
+++ tests/pyre.lib/grid/index_sanity.cc
@@ -0,0 +1,31 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2020 all rights reserved
+
+
+// get the grid
+#include <p2/grid.h>
+
+
+// type alias
+using rep_t = std::array<int, 4>;
+using idx_t = pyre::grid::index_t<rep_t, true>;
+
+
+// compile time sanity check: make sure the header file is accessible
+int main() {
+    // make an index
+    idx_t t { 0,0,0,0 };
+
+    // verify that the index dimensionality is reported correctly through the type
+    static_assert (idx_t::dim() == 4);
+    // verify that the index dimensionality is reported correctly through an instance
+    static_assert (t.dim() == 4);
+
+    // all done
+    return 0;
+}
+
+
+// end of file
