a3ac41d2775813a8bec3c4e638428c3f415ce38d
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-05-25 11:06:13 -0700

    pyre::memory: added test cases with out of bounds accesses

---
+++ tests/pyre.lib/memory/map_oob.cc
@@ -0,0 +1,46 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2020 all rights reserved
+
+
+// access the memory package
+#include <p2/memory.h>
+// support
+#include <cassert>
+
+
+// type aliases
+using cell_t = double;
+using map_t = pyre::memory::map_t<cell_t, true>;
+
+
+// create a map over an existing product in read-only mode
+int main(int argc, char * argv[]) {
+    // initialize the journal
+    pyre::journal::init(argc, argv);
+    pyre::journal::application("map_oob");
+
+    // silence the firewall
+    pyre::journal::firewall_t::quiet();
+
+    // open an existing file-backed memory block
+    map_t product("map.dat");
+
+    // gingerly
+    try {
+        // make an out-of-bounds access
+        product[product.cells()];
+        // unreachable
+        throw std::logic_error("unreachable");
+    // catch the firewall
+    } catch (const pyre::journal::firewall_t::exception_type &) {
+        // all good
+    }
+
+    // all done
+    return 0;
+}
+
+
+// end of file
