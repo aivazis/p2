2788a25231bf540f93fd3c9b9c1ebd9fbd6e25bf
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-06-18 09:11:57 +0300

    pyre::grid: added the forward and inverse packing map to {canonical_t}

--- lib/p2/grid/Canonical.h
+++ lib/p2/grid/Canonical.h
@@ -19,10 +19,15 @@ class pyre::grid::Canonical {
 public:
     // alias for me
     using canonical_type = Canonical<N>;
+    // the sizes of things
+    using size_type = size_t;
     // my parts
     using index_type = Index<N>;
     using shape_type = Shape<N>;
     using order_type = Order<N>;
+
+    // offsets
+    using difference_type = typename index_type::rep_type::difference_type;
 
     // metamethods
 public:
@@ -46,15 +51,25 @@ public:
     constexpr auto shape() const -> shape_type;
     constexpr auto order() const -> order_type;
     constexpr auto origin() const -> index_type;
-    constexpr auto nudge() const -> typename index_type::axis_type;
+    constexpr auto nudge() const -> difference_type;
 
+    // the total number of addressable cells
+    constexpr auto size() const -> size_type;
+
+    // the packing isomorphism
+    constexpr auto index(difference_type) -> index_type;
+    constexpr auto offset(const index_type &) -> difference_type;
+
+    // static interface
+public:
+    static constexpr auto dim() -> size_type;
 
     // implementation details: data
 private:
-    shape_type _shape;     // my shape
-    order_type _order;     // the packing order of the axes
-    index_type _origin;    // the smallest allowable index value
-    long _nudge;           // offset correction when {_origin} is not {zero}
+    shape_type _shape;         // my shape
+    order_type _order;         // the packing order of the axes
+    index_type _origin;        // the smallest allowable index value
+    difference_type _nudge;    // offset correction when {_origin} is not {zero}
 };
 
 
---
+++ tests/pyre.lib/grid/canonical_map.cc
@@ -0,0 +1,39 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2020 all rights reserved
+
+
+// support
+#include <cassert>
+// get the grid
+#include <p2/grid.h>
+
+
+// type alias
+using canonical_t = pyre::grid::canonical_t<3>;
+
+
+// simple check that the map from index space to offsets is correct
+int main() {
+    // pick a shape
+    canonical_t::shape_type shape { 2,3,4 };
+    // make a canonical packing strategy
+    canonical_t packing { shape };
+
+    // make an index
+    canonical_t::index_type index { 1, 2, 3 };
+    // get its offset
+    auto offset = packing.offset(index);
+    // map it back to an index
+    auto image = packing.index(offset);
+
+    // verify that the {image} is our original index
+    assert (image == index);
+
+    // all done
+    return 0;
+}
+
+
+// end of file
