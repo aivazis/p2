f61c2ae02daee22e7476943e5228caabb6b06309
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-06-20 20:32:24 +0300

    pyre::grid: decoupled {order_t} from {product_t}; despite the implementaion similarities, the association felt wrong

--- lib/p2/grid/Order.h
+++ lib/p2/grid/Order.h
@@ -9,18 +9,19 @@
 
 
 // generalization to multiple dimensions of the familiar row-major and column-major order
-// strategies. this is captured as a permutation in S_N that denotes the storage order of the
+// strategies. this is captured as a permutation in {S_N} that denotes the storage order of the
 // indices.
 template <pyre::grid::size_t N>
-class pyre::grid::Order : public Product<N, size_t> {
+class pyre::grid::Order {
     // types
 public:
     // alias for me
     using order_type = Order<N>;
-    // alias for my base
-    using product_type = Product<N, size_t>;
+    // my representation
+    using size_type = decltype(N);
+    using rep_type = array_t<size_t, N>;
     // individual ranks
-    using rank_type = typename product_type::rep_type::value_type;
+    using rank_type = typename rep_type::value_type;
     using rank_reference = rank_type &;
     using rank_const_reference = const rank_type &;
 
@@ -33,6 +34,32 @@ public:
     template <typename... argT>
     constexpr explicit Order(argT...);
 
+    // let the compiler write the rest
+    Order(const Order &) = default;
+    Order(Order &&) = default;
+    Order & operator=(const Order &) = default;
+    Order & operator=(Order &&) = default;
+
+    // interface
+public:
+    // access
+    // read only
+    constexpr auto operator[](size_type) const -> rank_type;
+    // read/write
+    constexpr auto operator[](size_type) -> rank_reference;
+
+    // iteration support
+    // read only
+    constexpr auto begin() const;
+    constexpr auto end() const;
+    // read/write
+    constexpr auto begin();
+    constexpr auto end();
+
+    // static interface
+public:
+    static constexpr auto dim() -> size_type;
+
     // static interface: factories
 public:
     // c-like: last index varies first, in order right to left
@@ -44,6 +71,10 @@ public:
     static constexpr auto columnMajor();
     // and its alias
     static constexpr auto fortran();
+
+    // implementation details: data
+private:
+    rep_type _ranks;
 };
 
 
