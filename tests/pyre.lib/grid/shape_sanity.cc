072eae2cb25c1993eac6f1c220ad317759000c10
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-06-23 15:14:02 +0300

    pyre::grid: upgraded the {shape_t} tests with compile time chacks wherever possible, and journal

--- tests/pyre.lib/grid/shape_sanity.cc
+++ tests/pyre.lib/grid/shape_sanity.cc
@@ -15,25 +15,38 @@ using shape_t = pyre::grid::shape_t<4>;
 
 
 // sanity check
-int main() {
+int main(int argc, char * argv[]) {
+    // initialize the journal
+    pyre::journal::init(argc, argv);
+    pyre::journal::application("shape_sanity");
+    // make a channel
+    pyre::journal::debug_t channel("pyre.grid.shape");
+
     // make a shape
-    shape_t s { 2, 3, 4, 5 };
+    constexpr shape_t s { 2, 3, 4, 5 };
+    // show me
+    channel
+        << "s: " << s << pyre::journal::newline
+        << "  rank: " << shape_t::rank() << "  (from the type)" << pyre::journal::newline
+        << "  rank: " << s.rank() << "  (from the instance)" << pyre::journal::newline
+        << "  capacity: " << s.capacity()
+        << pyre::journal::endl(__HERE__);
 
     // verify that the index dimensionality is reported correctly through the type
-    static_assert (shape_t::dim() == 4);
+    static_assert (shape_t::rank() == 4);
     // verify that the index dimensionality is reported correctly through an instance
-    static_assert (s.dim() == 4);
+    static_assert (s.rank() == 4);
 
     // verify that its capacity is equal to the product of the possible values along each axis
-    assert (s.size() == 2*3*4*5);
+    assert (s.capacity() == 2*3*4*5);
 
     // verify that a shape is equal to itself
-    assert (s == s);
+    static_assert (s == s);
 
     // make another
-    shape_t z { 0, 0, 0, 0 };
-    // that's not equal to {s}
-    assert (s != z);
+    shape_t z { 2, 3, 4, 5 };
+    // that's equal to {s}
+    assert (s == z);
 
     // all done
     return 0;
