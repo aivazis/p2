95a00a1e094bce7ee5d8e430049e6efdf846e0a8
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-06-23 11:58:51 +0300

    pyre::grid: enabled {journal} logging in the {order_t} test cases

--- tests/pyre.lib/grid/order_access.cc
+++ tests/pyre.lib/grid/order_access.cc
@@ -15,9 +15,20 @@ using order_t = pyre::grid::order_t<4>;
 
 
 // exercise operator[]
-int main() {
+int main(int argc, char * argv[]) {
+    // initialize the journal
+    pyre::journal::init(argc, argv);
+    pyre::journal::application("order_access");
+    // make a channel
+    pyre::journal::debug_t channel("pyre.grid.order");
+
     // make a constexpr index ordering
     constexpr order_t order_1 { 0, 1, 2, 3 };
+
+    // show me
+    channel
+        << "[0,1,2,3]: " << order_1
+        << pyre::journal::endl(__HERE__);
 
     // verify the contents are available at compile time
     static_assert (order_1[0] == 0);
@@ -33,6 +44,11 @@ int main() {
     order_2[1] = 1;
     order_2[2] = 2;
     order_2[3] = 3;
+
+    // show me
+    channel
+        << "[0,1,2,3]: " << order_2
+        << pyre::journal::endl(__HERE__);
 
     // check it
     assert (order_2[0] == 0);
--- tests/pyre.lib/grid/order_sanity.cc
+++ tests/pyre.lib/grid/order_sanity.cc
@@ -15,9 +15,22 @@ using order_t = pyre::grid::order_t<4>;
 
 
 // sanity check
-int main() {
+int main(int argc, char * argv[]) {
+    // initialize the journal
+    pyre::journal::init(argc, argv);
+    pyre::journal::application("order_access");
+    // make a channel
+    pyre::journal::debug_t channel("pyre.grid.order");
+
     // make an index ordering
     order_t shuffle { 0, 1, 2, 3 };
+
+    // show me
+    channel
+        << "an ordering: " << shuffle << pyre::journal::newline
+        << "  rank: " << order_t::rank() << "  (from the type)" << pyre::journal::newline
+        << "  rank: " << shuffle.rank() << "  (from the instance)"
+        << pyre::journal::endl(__HERE__);
 
     // verify that the dimensionality is reported correctly through the type
     static_assert (order_t::rank() == 4);
@@ -26,11 +39,19 @@ int main() {
 
     // make a column major ordering
     order_t fortran = order_t::fortran();
+    // show me
+    channel
+        << "fortran: " << fortran
+        << pyre::journal::endl(__HERE__);
     // check that it's equal to {shuffle}
     assert (shuffle == fortran);
 
     // make a column major ordering
     order_t c = order_t::c();
+    // show me
+    channel
+        << "c: " << c
+        << pyre::journal::endl(__HERE__);
     // check that it's different from {shuffle}
     assert (!(shuffle == c));
 
