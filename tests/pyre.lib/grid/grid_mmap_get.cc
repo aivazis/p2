6caf8953488f26709195ef81613f0e0e60a65144
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-07-03 13:55:20 +0300

    pyre::grid: added test cases exercising a {grid_t} over {map_t}

---
+++ tests/pyre.lib/grid/grid_mmap_get.cc
@@ -0,0 +1,49 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2020 all rights reserved
+
+
+// support
+#include <p2/memory.h>
+// get the grid
+#include <p2/grid.h>
+
+
+// sanity check: make sure we can instantiate a grid
+int main(int argc, char * argv[]) {
+    // initialize the journal
+    pyre::journal::init(argc, argv);
+    pyre::journal::application("grid_mmap_get");
+    // make a channel
+    pyre::journal::debug_t channel("pyre.grid.mmap");
+
+    // we'll work with a 3d conventionally packed grid
+    using pack_t = pyre::grid::canonical_t<3>;
+    // of doubles on the heap
+    using storage_t = pyre::memory::constmap_t<double>;
+    // putting it all together
+    using grid_t = pyre::grid::grid_t<pack_t, storage_t>;
+
+    // packing: 2x3x4
+    pack_t packing { {1024,1024, 8} };
+    // instantiate the grid
+    grid_t grid { packing, std::make_shared<storage_t>("grid_mmap.data") };
+
+    // show me the value at the origin
+    channel
+        << "grid[0,0,0] = " << grid[{0,0,0}]
+        << pyre::journal::endl(__HERE__);
+
+    // go through it in packing order
+    for (const auto & idx : grid) {
+        // and verify
+        assert(( grid[idx] == grid.layout()[idx] ));
+    }
+
+    // nothing to do
+    return 0;
+}
+
+
+// end of file
