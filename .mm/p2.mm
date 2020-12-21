d083752deb1e400fa742d759eafebbb39ef344be
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-12-21 14:34:42 -0800

    p2: retired the extension

--- .mm/p2.mm
+++ .mm/p2.mm
@@ -9,25 +9,11 @@ p2.packages := p2.pkg
 # libraries
 p2.libraries :=
 # python extensions
-p2.extensions := p2.ext
+p2.extensions :=
 # docker images
 p2.docker-images := p2.focal-gcc p2.focal-clang p2.eoan-gcc p2.eoan-clang
 # and test suites
 p2.tests := p2.pkg.tests p2.ext.tests
-
-
-# the p2 extension settings
-# no cpsules to export with {pybind11}
-p2.ext.capsule :=
-# wrap the pyre library
-p2.ext.wraps := p2.lib
-# dependencies to other project parts
-p2.ext.prerequisites :=  p2.pkg p2.lib
-# external dependencies
-p2.ext.extern := p2.lib pyre pybind11 python
-# compiler control
-p2.ext.lib.c++.defines += PYRE_CORE
-p2.ext.lib.c++.flags += $($(compiler.c++).std.c++17)
 
 
 # the docker images
